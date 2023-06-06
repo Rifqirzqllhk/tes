95fc0e03b3d2792ea0129b5cc21693f2bd7a092f
Hernanda Dzaki Permana <hernanda03@gmail.com>
2023-06-06 18:18:13 +0700

    buat koding Controller, model, dan route fitur Pesanan

--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Pesanan extends Model
 {
     use HasFactory;
+
+    use HasFactory;
+    protected $guarded = ['id'];
+  
+    public function buku()
+    {
+      return $this->belongsTo(buku::class);
+    }
+  
+    public function user()
+    {
+      return $this->belongsTo(user::class);
+    }
 }
